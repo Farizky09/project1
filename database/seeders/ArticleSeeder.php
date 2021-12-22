21e5acd0ab574cb2af43e513195b69fbe422f350
muhammadjafar <mjafar747@gmail.com>
2021-12-22 13:16:01 +0700

    add seeder

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -4,7 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Article;
+use App\Models\Tag;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class ArticleSeeder extends Seeder
 {
@@ -15,9 +17,24 @@ class ArticleSeeder extends Seeder
      */
     public function run()
     {
-       Article::create([
-           
-           'judul' => 'mencoba lagi'
-       ]);
+        Schema::disableForeignKeyConstraints();
+        Article::truncate();
+        $article = Article::create([
+            'judul' => 'Habis Gelap Terbitlah Terang'
+        ]);
+
+        Tag::create([
+            'tag' => 'surat',
+            'article_id'    => $article->id
+        ]);
+        Tag::create([
+            'tag' => 'sejarah',
+            'article_id'    => $article->id
+        ]);
+        Tag::create([
+            'tag' => 'diksi',
+            'article_id'    => $article->id
+        ]);
+        Schema::enableForeignKeyConstraints();
     }
 }
